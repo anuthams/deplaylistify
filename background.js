4aaa82762a263bca3bc0f97c15797e77dfa1b8fa
anuthams <36064937+anuthams@users.noreply.github.com>
2025-08-14 23:51:03 +1200

    Fix bug causing incorrect tab icon on tab reload
    Added a new default icon when the extension isn't actually active
    or inactive to more clearly communicate when the app is initialising.
    Also add a check for when the tab url changes.
    Tidy up of background.js functions for ease of use and readability.

--- background.js
+++ background.js
@@ -5,12 +5,24 @@ const STATE = {
     DISABLED: false
 }
 
+function consoleError(messageString) {
+    console.error(messageString);
+    return
+}
+
+function getTabStates() {
+    return JSON.parse(localStorage.getItem("tabExtensionState"));
+};
+
+function setTabStates(new_states) {
+    localStorage.setItem("tabExtensionState", JSON.stringify(new_states));
+};
+
 async function getTabIDs() {
     return browser.tabs.query({}).then((tabs) => {
         return tabs.map((tab) => tab.id);
     });
 };
-
 
 function setTabIcon(tab_id, state) {
     console.debug(`Updating icon for ${tab_id}`);
@@ -24,13 +36,11 @@ function setTabIcon(tab_id, state) {
     });
 }
 
-
 //Run this once per intialisation
 browser.runtime.onInstalled.addListener(() => {
-    // State object tracking icon status per tab (use let so we can reassign for immutability)
     console.debug("background.js - Running init")
     getTabIDs().then((tabIDs) => {
-        let stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+        let stored_state = getTabStates();
         if (stored_state === null) stored_state = {};
         new_state = tabIDs.reduce((accumulate, current) => {
             // if the stored_state has an item that is not in current tabs, it's removed. if the current tabs has an item not in stored, it is initialised to isDisabled
@@ -38,7 +48,7 @@ browser.runtime.onInstalled.addListener(() => {
             setTabIcon(current, accumulate[current]);
             return accumulate;
         }, {})
-        localStorage.setItem("tabExtensionState", JSON.stringify(new_state));
+        setTabStates(new_state);
     });
 });
 
@@ -46,9 +56,9 @@ browser.runtime.onInstalled.addListener(() => {
 // Toggle icon state per tab
 browser.action.onClicked.addListener((tab) => {
     console.log("background.js Action button clicked!")
-    const tab_id = tab.id;
 
-    stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+    const tab_id = tab.id;
+    stored_state = getTabStates();
     if (!stored_state) {
         console.debug("background.js:28 tabExtensionState doesn't exist");
         return // exit early, shouldn't happen but guard rails amirite
@@ -57,41 +67,55 @@ browser.action.onClicked.addListener((tab) => {
     // toggle state of current tab
     new_state = !stored_state[tab_id];
     stored_state[tab_id] = new_state;
-    localStorage.setItem("tabExtensionState", JSON.stringify(stored_state));
+    setTabStates(stored_state);
     browser.tabs.sendMessage(tab_id, { "new_state": new_state }).catch((error => {
-        console.log(`deplaylistify: tab ${tab_id} not on YT`)
+        console.log(`deplaylistify: tab ${tab_id} not on YT`);
     })); // catch error if not on yt
     setTabIcon(tab_id, new_state);
 });
 
 browser.tabs.onCreated.addListener((tab) => {
     console.debug("New tab opened, adding to state tracking.");
-    stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+    stored_state = getTabStates();
     stored_state[tab.id] = STATE.ENABLED;
     setTabIcon(tab.id, STATE.ENABLED);
-    localStorage.setItem("tabExtensionState", JSON.stringify(stored_state));
+    setTabStates(stored_state);
 });
 
 browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    console.debug(`Tab ${tabId} removed, removing from state tracking`)
-    stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+    console.debug(`Tab ${tabId} removed, updating tab states`);
+    stored_state = getTabStates();
     delete stored_state[tabId];
-    localStorage.setItem("tabExtensionState", JSON.stringify(stored_state));
+    setTabStates(stored_state);
 });
+
+
+async function updateForeground() {
+    //Run this to send updates to the foreground scripts - either on a tab update
+}
+
+browser.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
+    console.debug(`background.js - tab ${tabID} updated`);
+    stored_state = getTabStates();
+    if (!stored_state.hasOwnProperty(tabID)) consoleError(`No state info for tab ${tabID}`);
+    if (changeInfo.status === "complete") setTabIcon(tabID, stored_state[tabID])
+    if (changeInfo.url) console.debug(`background.js - tab URL changed to ${changeInfo.url}`)
+})
+
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.hasOwnProperty('init') && message['init']) {
         console.log(`Initialised ${sender.tab.id}`);
-        stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+        stored_state = getTabStates();
         sendResponse({ current_state: stored_state[sender.tab.id] });
     }
 
     else if (message.hasOwnProperty('new_state') && message['new_state'] !== undefined) {
         console.log(`Toggling deplaylistify on tab ${sender.tab.id}`);
         new_state = message['new_state'];
-        stored_state = JSON.parse(localStorage.getItem("tabExtensionState"));
+        stored_state = getTabStates();
         stored_state[sender.tab.id] = new_state;
-        localStorage.setItem("tabExtensionState", JSON.stringify(stored_state));
+        setTabStates(stored_state);
         setTabIcon(sender.tab.id, stored_state[sender.tab.id]);
     }
 });
